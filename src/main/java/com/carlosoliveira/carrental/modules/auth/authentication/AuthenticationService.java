04bf85282a6cd685c684407f8d591fad0d2a4fbb
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-22 20:36:44 -0300

    feat(authentication-service): ensure sign-up creates a new user

---
+++ src/main/java/com/carlosoliveira/carrental/modules/auth/authentication/AuthenticationService.java
@@ -0,0 +1,26 @@
+package com.carlosoliveira.carrental.modules.auth.authentication;
+
+import com.carlosoliveira.carrental.modules.auth.authentication.inputs.SignUpInput;
+import com.carlosoliveira.carrental.modules.user.application.ports.UserRepository;
+import com.carlosoliveira.carrental.modules.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public void signUp(SignUpInput signUpInput) {
+        User user = new User();
+        user.setEmail(signUpInput.email());
+        user.setFirstName(signUpInput.firstName());
+        user.setLastName(signUpInput.lastName());
+        user.setUsername(signUpInput.username());
+        user.setPassword(passwordEncoder.encode(signUpInput.password()));
+        userRepository.save(user);
+    }
+}
---
+++ src/test/java/com/carlosoliveira/carrental/modules/auth/authentication/AuthenticationServiceTest.java
@@ -0,0 +1,53 @@
+package com.carlosoliveira.carrental.modules.auth.authentication;
+
+import com.carlosoliveira.carrental.modules.auth.authentication.inputs.SignUpInput;
+import com.carlosoliveira.carrental.modules.user.application.ports.UserRepository;
+import com.carlosoliveira.carrental.modules.user.domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private AuthenticationService sut;
+
+    private SignUpInput signUpInput;
+
+    @BeforeEach
+    void setUp() {
+        signUpInput = new SignUpInput(
+                "any_username",
+                "any first name",
+                "any last name",
+                "password",
+                "test@example.com",
+                "+5522999999999"
+        );
+    }
+
+    @Test
+    void creates_a_new_user() {
+        // Arrange
+        when(passwordEncoder.encode(signUpInput.password())).thenReturn("hashed_password");
+
+        // Act
+        sut.signUp(signUpInput);
+
+        // Assert
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+}

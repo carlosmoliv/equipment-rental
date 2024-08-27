0701b05f27ea5c9e5e17b2ec6f070ce81a971f75
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-08-27 16:54:09 -0300

    fix: add repository annotation to UserRepository interface

--- src/main/java/com/carlosoliveira/carrental/modules/user/application/ports/UserRepository.java
+++ src/main/java/com/carlosoliveira/carrental/modules/user/application/ports/UserRepository.java
@@ -1,9 +1,11 @@
 package com.carlosoliveira.carrental.modules.user.application.ports;
 
 import com.carlosoliveira.carrental.modules.user.domain.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository {
     void save(User user);
     Optional<User> findByEmail(String email);

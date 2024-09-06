790a2baa7374adf31dec59f3f992f43fef422306
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-05 23:51:01 -0300

    refactor: make UserRepository extends JpaRepository

--- src/main/java/com/carlosoliveira/equipment_rental/modules/user/application/ports/UserRepository.java
+++ src/main/java/com/carlosoliveira/equipment_rental/modules/user/application/ports/UserRepository.java
@@ -1,12 +1,13 @@
 package com.carlosoliveira.equipment_rental.modules.user.application.ports;
 
 import com.carlosoliveira.equipment_rental.modules.user.domain.User;
+import com.carlosoliveira.equipment_rental.modules.user.infra.postgres.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository {
-    void save(User user);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<User> findByEmail(String email);
 }

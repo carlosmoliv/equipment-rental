d885a90987042b760c8cfff7fea44677bef8b162
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-11 21:17:21 -0300

    test(integration): ensure sign-up endpoint creates a new user successfully

---
+++ src/test/java/com/carlosoliveira/equipment_rental/modules/auth/authentication/AuthenticationIntegrationTest.java
@@ -0,0 +1,75 @@
+package com.carlosoliveira.equipment_rental.modules.auth.authentication;
+
+import com.carlosoliveira.equipment_rental.modules.auth.authentication.dtos.SignUpDto;
+import com.carlosoliveira.equipment_rental.modules.user.application.ports.UserRepository;
+import com.github.javafaker.Faker;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthenticationIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres");
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private SignUpDto signUpDto;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    Faker faker = new Faker();
+
+    @BeforeEach
+    public void init() {
+        String password = faker.internet().password();
+        signUpDto = new SignUpDto(
+                faker.name().username(),
+                faker.name().firstName(),
+                faker.name().lastName(),
+                "any_email@email.com",
+                faker.phoneNumber().toString(),
+                password,
+                password
+        );
+    }
+
+    @Test
+    void connection_established()  {
+        Assertions.assertThat(postgres.isCreated()).isTrue();
+        Assertions.assertThat(postgres.isRunning()).isTrue();
+    }
+
+    @Nested
+    class SignUpTests {
+
+        @Test
+        void sign_up_succeeds() throws Exception {
+            // Act
+            ResponseEntity<String> response = restTemplate.postForEntity("/api/authentication/sign-up", signUpDto, String.class);
+
+            // Assert
+            Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+            assertTrue(userRepository.findByEmail(signUpDto.email()).isPresent());
+        }
+    }
+}

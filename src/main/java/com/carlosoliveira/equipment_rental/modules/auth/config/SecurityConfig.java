787a87037be91bede978574687fb40d83173ee69
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-11 21:03:45 -0300

    feat: add security filter chain base setup

--- src/main/java/com/carlosoliveira/equipment_rental/modules/auth/config/SecurityConfig.java
+++ src/main/java/com/carlosoliveira/equipment_rental/modules/auth/config/SecurityConfig.java
@@ -2,11 +2,23 @@ package com.carlosoliveira.equipment_rental.modules.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/authentication/**").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

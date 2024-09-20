b21f856d1c3a70198f834b3c463b71d539685725
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-19 21:20:21 -0300

    feat(auth-service): ensure login returns a token when user has valid credentials

--- src/main/java/com/carlosoliveira/equipment_rental/modules/iam/authentication/AuthenticationService.java
+++ src/main/java/com/carlosoliveira/equipment_rental/modules/iam/authentication/AuthenticationService.java
@@ -1,12 +1,16 @@
 package com.carlosoliveira.equipment_rental.modules.iam.authentication;
 
 import com.carlosoliveira.equipment_rental.modules.iam.authentication.exceptions.EmailAlreadyInUseException;
+import com.carlosoliveira.equipment_rental.modules.iam.authentication.inputs.SignInInput;
 import com.carlosoliveira.equipment_rental.modules.iam.authentication.inputs.SignUpInput;
 import com.carlosoliveira.equipment_rental.modules.iam.ports.HashingService;
+import com.carlosoliveira.equipment_rental.modules.iam.ports.TokenService;
 import com.carlosoliveira.equipment_rental.modules.user.application.ports.UserRepository;
 import com.carlosoliveira.equipment_rental.modules.user.domain.User;
 import com.carlosoliveira.equipment_rental.modules.user.domain.factories.UserFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,6 +22,7 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final HashingService hashingService;
     private final UserFactory userFactory;
+    private final TokenService tokenService;
 
     public void signUp(SignUpInput signUpInput) {
         Optional<User> userExists = userRepository.findByEmail(signUpInput.email());
@@ -26,5 +31,17 @@ public class AuthenticationService {
         }
         User user = userFactory.createUser(signUpInput, hashingService);
         userRepository.save(user);
+    }
+
+
+    public String login(SignInInput signInInput) {
+        User user = userRepository.findByEmail(signInInput.email())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+
+        if (!hashingService.matches(signInInput.password(), user.getPassword())) {
+            throw new BadCredentialsException("Invalid credentials.");
+        }
+
+        return tokenService.generate(user);
     }
 }
--- src/test/java/com/carlosoliveira/equipment_rental/modules/iam/authentication/AuthenticationServiceTest.java
+++ src/test/java/com/carlosoliveira/equipment_rental/modules/iam/authentication/AuthenticationServiceTest.java
@@ -1,13 +1,15 @@
 package com.carlosoliveira.equipment_rental.modules.iam.authentication;
 
 import com.carlosoliveira.equipment_rental.modules.iam.authentication.exceptions.EmailAlreadyInUseException;
+import com.carlosoliveira.equipment_rental.modules.iam.authentication.inputs.SignInInput;
 import com.carlosoliveira.equipment_rental.modules.iam.authentication.inputs.SignUpInput;
 import com.carlosoliveira.equipment_rental.modules.iam.ports.HashingService;
+import com.carlosoliveira.equipment_rental.modules.iam.ports.TokenService;
 import com.carlosoliveira.equipment_rental.modules.user.application.ports.UserRepository;
 import com.carlosoliveira.equipment_rental.modules.user.domain.User;
 import com.carlosoliveira.equipment_rental.modules.user.domain.factories.UserFactory;
-import com.carlosoliveira.equipment_rental.modules.user.infra.postgres.entities.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -28,15 +31,21 @@ class AuthenticationServiceTest {
     @Mock
     private HashingService hashingService;
 
+    @Mock
+    private TokenService tokenService;
+
     @InjectMocks
     private AuthenticationService sut;
 
     private SignUpInput signUpInput;
+    private SignInInput signInInput;
+    private User user;
+
 
     @BeforeEach
     void setUp() {
         UserFactory userFactory = new UserFactory();
-        sut = new AuthenticationService(userRepository, hashingService, userFactory);
+        sut = new AuthenticationService(userRepository, hashingService, userFactory, tokenService);
         signUpInput = new SignUpInput(
                 "any_username",
                 "any first name",
@@ -45,29 +54,59 @@ class AuthenticationServiceTest {
                 "test@example.com",
                 "+5522999999999"
         );
+
+        signInInput = new SignInInput(
+                "test@example.com",
+                "password"
+        );
+
+        user = new User();
+        user.setEmail(signInInput.email());
+        user.setPassword("hashed_password");
     }
 
-    @Test
-    void creates_a_new_user() {
-        // Arrange
-        String hashedPassword = "hashed_password";
-        when(hashingService.encode(signUpInput.password())).thenReturn(hashedPassword);
-        when(userRepository.findByEmail(signUpInput.email())).thenReturn(Optional.empty());
+    @Nested
+    class SignUpTests {
+        @Test
+        void creates_a_new_user() {
+            // Arrange
+            String hashedPassword = "hashed_password";
+            when(hashingService.encode(signUpInput.password())).thenReturn(hashedPassword);
+            when(userRepository.findByEmail(signUpInput.email())).thenReturn(Optional.empty());
 
-        // Act
-        sut.signUp(signUpInput);
+            // Act
+            sut.signUp(signUpInput);
 
-        // Assert
-        verify(userRepository, times(1)).save(any(User.class));
+            // Assert
+            verify(userRepository, times(1)).save(any(User.class));
+        }
+
+        @Test
+        void fails_when_email_already_in_use() {
+            // Arrange
+            User existingUser = new User();
+            when(userRepository.findByEmail(signUpInput.email())).thenReturn(Optional.of(existingUser));
+
+            // Act & Assert
+            assertThrows(EmailAlreadyInUseException.class, () -> sut.signUp(signUpInput));
+        }
     }
 
-    @Test
-    void fails_when_email_already_in_use() {
-        // Arrange
-        User existingUser = new User();
-        when(userRepository.findByEmail(signUpInput.email())).thenReturn(Optional.of(existingUser));
+    @Nested
+    class SignInTests {
+        @Test
+        void returns_token_when_credentials_are_valid() {
+            // Arrange
+            when(userRepository.findByEmail(signInInput.email())).thenReturn(Optional.of(user));
+            when(hashingService.matches(signInInput.password(), user.getPassword())).thenReturn(true);
+            when(tokenService.generate(user)).thenReturn("token");
 
-        // Act & Assert
-        assertThrows(EmailAlreadyInUseException.class, () -> sut.signUp(signUpInput));
+            // Act
+            String token = sut.login(signInInput);
+
+            // Assert
+            assertEquals("token", token);
+            verify(tokenService, times(1)).generate(user);
+        }
     }
 }

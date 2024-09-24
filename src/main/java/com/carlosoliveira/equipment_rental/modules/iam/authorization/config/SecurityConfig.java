8df7b29b3fbb636b5c52737022e59035c99839a2
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-09-24 11:29:31 -0300

    reactor: update SecurityConfig

--- src/main/java/com/carlosoliveira/equipment_rental/modules/iam/authorization/config/SecurityConfig.java
+++ src/main/java/com/carlosoliveira/equipment_rental/modules/iam/authorization/config/SecurityConfig.java
@@ -2,22 +2,31 @@ package com.carlosoliveira.equipment_rental.modules.iam.authorization.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/api/authentication/**").permitAll()
-                        .anyRequest().authenticated());
-        return http.build();
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 
     @Bean

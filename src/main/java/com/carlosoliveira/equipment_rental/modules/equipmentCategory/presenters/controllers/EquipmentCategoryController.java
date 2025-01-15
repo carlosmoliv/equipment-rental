ab874bf9d66e7622e7fcf43db735100c9ff77892
Carlos Oliveira <carlosmoliveira93@gmail.com>
2025-01-15 20:39:10 -0300

    feat(category): add admin pre-authorization for managing equipment categories

--- src/main/java/com/carlosoliveira/equipment_rental/modules/equipmentCategory/presenters/controllers/EquipmentCategoryController.java
+++ src/main/java/com/carlosoliveira/equipment_rental/modules/equipmentCategory/presenters/controllers/EquipmentCategoryController.java
@@ -8,12 +8,14 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@PreAuthorize("hasRole('ADMIN')")
 @RequiredArgsConstructor
-@RestController()
+@RestController
 @RequestMapping("/api/categories")
 public class EquipmentCategoryController {
 
